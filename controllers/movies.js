51cf4e51d9f93eb61b37981e9a89ce254b652f64
Alan Quintana <a338698@uach.mx>
2023-05-03 13:16:30 -0600

    Update corregido

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const {Movie} = require('../db');
+const {Actor} = require('../db');
 
 function list(req, res, next){
-    res.send('respond with a list');  
+    Movie.findAll({include:['genre','director','actors']})
+        .then(objects => res.json(objects))
+        .catch(err => res.send(err));
 };
 
 function index(req, res, next){
@@ -9,21 +13,67 @@ function index(req, res, next){
 };
 
 function create(req, res, next){
-    let name = req.body.name;
-    let lastname = req.body.lastname;
-    res.send(`respond with a a created name = ${name} and lastname = ${lastname}`);  
+    const title = req.body.title;
+    const genreId = req.body.genreId;
+    const directorId = req.body.directorId;
+    
+    
+    let movie = new Object({
+        title:title,
+        genreId:genreId,
+        directorId:directorId
+    });
+
+    Movie.create(movie)
+        .then(obj=>res.json(obj))
+        .catch(err => res.send(err));
+
 };
 
 function replace(req, res, next){
-    res.send(`respond with a replace =${req.params.id}`);  
+    const id = req.params.id;
+    Movie.findByPk(id)
+            .then((object)=>{
+                const genreId = req.body.genreId ? req.body.genreId :"";
+                const directorId = req.body.directorId ? req.body.directorId :"";
+                object.update({genreId:genreId,directorId:directorId})
+                .then(obj => res.json(obj))
+                .catch(err => res.send(err));
+            })
+            .catch(err => res.send(err));
 };
 
 function update(req, res, next){
-    res.send(`respond with an update =${req.params.id}`);  
+    Movie.findByPk(id)
+    .then((object)=>{
+        const genreId = req.body.genreId ? req.body.genreId :object.genreId;
+        const directorId = req.body.directorId ? req.body.directorId :object.directorId;
+        object.update({genreId:genreId,directorId:directorId})
+        .then(obj => res.json(obj))
+        .catch(err => res.send(err));
+    })
+    .catch(err => res.send(err));
 };
 
 function destroy(req, res, next){
-    res.send(`respond with a destroy =${req.params.id}`);  
+    const id = req.params.id;
+    Movie.destroy({where:{id:id}})
+        .then(obj=res.json(obj))
+        .catch(err => res.send(err));
 };
 
-module.exports = {list,index,create,update,destroy,replace};
+function addActor(req,res,next){
+    const idMovie = req.body.idMovie;
+    const idActor = req.body.idActor;
+
+
+    Movie.findByPk(idMovie).then(movie => {
+            Actor.findByPk(idActor)
+                .then(actor=>{
+                    movie.addActor(actor)
+                    res.json(movie);
+                })
+}).catch(err => res.send(err));
+}
+
+module.exports = {list,index,create,update,destroy,replace,addActor};
