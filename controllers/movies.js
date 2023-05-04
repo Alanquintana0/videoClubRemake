17d1a8f188812aea03a997e04e481143b2b560bf
Alan Quintana <a338698@uach.mx>
2023-05-04 15:51:56 -0600

    Controladores actualizados

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,29 +1,136 @@
+const { json } = require('express');
 const express = require('express');
+const Director = require('../models/director');
+const Movie = require('../models/movie');
+const Actor = require('../models/actor');
+const Genre = require('../models/genre');
+
+
 
 function list(req, res, next){
-    res.send('respond with a list');  
+    Movie.find().populate("_director").then(objs => res.status(200).json({
+        message: "Listado de peliculas",
+        obj: objs
+    })).catch( ex => res.status(500).json({
+        message: "No se pudieron listar las peliculas",
+        obj: ex
+    }));
 };
 
 function index(req, res, next){
-    res.send(`respond with a index = ${req.params.id}`);  
+    const id = req.params.id;
+    Movie.findOne({"_id":id}).then(obj=>res.status(200).json({
+        message:`Movie con id ${id}`,
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message:"No info",
+        err:ex
+    }))
 };
 
-function create(req, res, next){
-    let name = req.body.name;
-    let lastname = req.body.lastname;
-    res.send(`respond with a a created name = ${name} and lastname = ${lastname}`);  
+async function create(req, res, next){
+    const title = req.body.title;
+    const directorId = req.body.directorId;
+    const genreId = req.body.genreId;
+    const actorId = req.body.actorId;
+
+    let director = await Director.findOne({"_id":directorId})
+    let genre = await Genre.findOne({"_id":genreId});
+    let actor = await Actor.findOne({"_id":actorId});
+    
+    let movie = new Movie({
+        title:title,
+        director:director,
+        genre:genre,
+        actor:actor
+    })
+
+    movie.save().then(obj => res.status(200).json({
+        message: "Pelicula creada correctamente",
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message: "No se ha logrado crear la pelicula",
+        err: ex
+    }))
 };
 
-function replace(req, res, next){
-    res.send(`respond with a replace =${req.params.id}`);  
+async function replace(req, res, next){
+    const id = req.params.id;
+    let title = req.body.title ? req.body.title:"";
+    let directorId = req.body.directorId ? req.body.directorId:"";
+    let genreId = req.body.genreId ? req.body.genreId:"";
+    let actorId = req.body.actorId ? req.body.actorId:"";
+
+    let director = await Director.findOne({"id":directorId});
+    let genre = await Genre.findOne({"_id":genreId});
+    let actor = await Actor.findOne({"_id":actorId});
+
+    let movie = new Object({
+        _title:title,
+        _director:director,
+        _genre:genre,
+        _actor:actor
+    })
+    Movie.findByIdAndUpdate({"_id":id},movie,{new:true})
+         .then(obj=> {res.status(200).json({
+            message:"Movie updated",
+            obj:obj
+         })}).catch(ex => res.status(500).json({
+            message:"Movie not pudated"
+         }))
+    
 };
 
-function update(req, res, next){
-    res.send(`respond with an update =${req.params.id}`);  
+async function add_actor_to_cast(actorId){
+    actor = await Actor.findOne({"_id":actorId});
+    actors.push(actor)
+    return actors
+};
+
+async function update(req, res, next){
+    
+    let id = req.params.id;
+    let title = req.body.title
+    let directorId = req.body.directorId
+    let genreId = req.body.genreId
+    let actorId = req.body.actorId
+
+    let movie = new Object();
+
+    director = await Director.findOne({"_id":directorId});
+    genre = await Genre.findOne({"_id":genreId});
+    actors = add_actor_to_cast(actorId);
+    
+
+    if(title){
+        movie._title = title;
+    }
+    if(genre){
+        movie._genre = genre;
+    }
+    if(actor){
+        movie._actor = actors;
+    }
+    
+    Movie.findOneAndUpdate({"_id":id},movie)
+         .then(obj=>res.status(200).json({
+            message:"Movie updates",
+            obj:obj
+         })).catch(ex => res.status(500).json({
+            message:"Movie not updated",
+            err:ex
+         }))
 };
 
 function destroy(req, res, next){
-    res.send(`respond with a destroy =${req.params.id}`);  
+    const id = req.params.id;
+    Movie.findByIdAndRemove({"_id":id}).then(obj=>res.status(200).json({
+        message:"Movie deleted",
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message:"Movie not deleted",
+        err:ex
+    }))
 };
 
 module.exports = {list,index,create,update,destroy,replace};
--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,92 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
-function list(req, res, next){
-    res.send('respond with a list');  
-};
-
-function index(req, res, next){
-    res.send(`respond with a index = ${req.params.id}`);  
-};
-
-function create(req, res, next){
+async function create(req, res, next) {
+    
     let name = req.body.name;
-    let lastname = req.body.lastname;
-    res.send(`respond with a a created name = ${name} and lastname = ${lastname}`);  
-};
+    let lastname = req.body.lastName;
+    let email = req.body.email;
+    let password = req.body.password;
 
-function replace(req, res, next){
-    res.send(`respond with a replace =${req.params.id}`);  
-};
+    //Generar el salt con las iteraciones para generar la cadena
+    const salt = await bcrypt.genSalt(10);
 
-function update(req, res, next){
-    res.send(`respond with an update =${req.params.id}`);  
-};
+    const passwordHash = await bcrypt.hash(password, salt);
 
-function destroy(req, res, next){
-    res.send(`respond with a destroy =${req.params.id}`);  
-};
+    let user = new User({
+        name: name,
+        lastname: lastname,
+        email: email,
+        password: passwordHash,
+        salt: salt
+    });
 
-module.exports = {list,index,create,update,destroy,replace};
+    user.save().then(obj => res.status(200).json({
+        message: "Usuario creado correctamente",
+        obj: obj
+        })).catch(ex => res.status(500).json({
+            message: "No se pudo almacenar el usuario",
+            obj: ex
+        }));
+}
+
+
+async function update(req, res, next) {
+    try{
+        const userId = req.params.id;
+        const {currerntPassword, newPassword} = req.body;
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(400).json({
+                message:"User not found"
+            });
+        }
+        const isMatch = await bcrypt.compare(currerntPassword,user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message:"Password is not correct"
+            })
+
+        }
+        const salt = await bcrypt.genSalt(10);
+        const newPasswordHash = await bcrypt.hash(newPassword,salt);
+        await User.updateOne({"_id":userId},{password:newPasswordHash,salt:salt})
+        res.status(200).json({
+            message:"Password Updated"
+        })
+    }catch( err ){
+        res.status(500).json({
+            message:"Error in update",
+            ex:err
+        });
+    }
+}
+
+async function destroy(req, res, next) {
+    try {
+        const userId = req.params.id;
+        const result = await User.deleteOne({_id: userId});
+        if (result.deletedCount === 1) {
+            res.status(200).json({
+                message: "Usuario eliminado correctamente"
+            });
+        } else {
+            res.status(404).json({
+                message: "No se encontró el usuario con el id proporcionado"
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al eliminar el usuario",
+            obj: error
+        });
+    }
+    
+}
+
+module.exports = { 
+    create,
+    update,
+    destroy
+};
